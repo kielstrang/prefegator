23e3ede4e53d5b5e6072378aa904f839309d7088
vagrant <kiel.strang@gmail.com>
2017-09-28 02:18:31 +0000

    Add data-helpers.js, create polls and options

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -1,13 +1,19 @@
 exports.seed = function(knex, Promise) {
   return knex('polls').del()
-    .then(function() {
-      return knex('polls').insert({ creator_email: 'gator@prefegator.com', name: 'Which is the best animal?' })
-        .then(function() {
-          return Promise.all([
-            knex('poll_options').insert({ poll_id: 1, option_name: 'Alligator' }),
-            knex('poll_options').insert({ poll_id: 1, option_name: 'Crocodile' }),
-            knex('poll_options').insert({ poll_id: 1, option_name: 'Caiman' }),
-          ]);
-        });
+    .then(() => {
+      const db = require('../../lib/data-helpers.js')(knex);
+
+      const poll = {
+        name: 'Which is the best animal?',
+        email: 'gator@prefegator.com',
+        desc: '',
+        options: [
+          { name: 'Alligator', desc: '' },
+          { name: 'Crocodile', desc: '' },
+          { name: 'Caiman', desc: '' }
+        ]
+      };
+
+      return db.createPoll(poll);
     });
 };
