b76973b43a112dc60d5130e8b9ea9b336daabe20
vagrant <kiel.strang@gmail.com>
2017-09-29 23:11:41 +0000

    Connect voting routes

--- routes/polls.js
+++ routes/polls.js
@@ -29,21 +29,22 @@ module.exports = (db) => {
   router.get("/:id", (req, res) => {
     db.getPoll(req.params.id)
       .then(poll => {
+        console.log(poll);
         res.locals.poll = poll;
+        res.locals.id = req.params.id;
         res.render('vote');
       });
   });
 
 
   router.post("/:id", (req, res) => {
-    const ballot = JSON.parse(req.body.poll);
-    console.log(ballot);
-    // db.saveBallot(req.params.id, ballot);
-    //submit vote
+    const ballot = JSON.parse(req.body.ballot);
+    db.saveBallot(req.params.id, ballot);
     res.redirect(`/polls/${req.params.id}/results`);
   });
 
   router.get("/:id/links", (req, res) => {
+    res.locals.id = req.params.id;
     res.render('links');
   });
 
