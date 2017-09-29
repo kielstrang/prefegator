404937ad32035a3d1bce4e507bdeb67fe6a2700f
cjwsstrm <cjwesstrom@gmail.com>
2017-09-29 23:59:32 +0000

    improved delete button functionality, now works on new items as well

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,6 +11,7 @@
 
 
 $(function() {
+  
   $('.add-option').on('click', function() {
     $('#create-option-form').submit();
   });
@@ -22,7 +23,7 @@ $(function() {
     const $divColName = $('<div>').addClass('col');
     const $spanName = $('<span>').addClass('item-name');
     const $divColDelete = $('<div>').addClass('col');
-    const $buttonDelete = $('<i>').addClass('list-buttons fa fa-minus-square-o fa-2x');
+    const $buttonDelete = $('<i>').addClass('list-buttons fa fa-minus-square-o fa-2x delete-option');
 
     $spanName.text($('.add-option-name').val());
 
@@ -32,14 +33,11 @@ $(function() {
     $list.append($divRow);
     $list.insertBefore('.list-add-option');
 
-
     $('.add-option-name').val('');
   })
 
-  $('.delete-option').on('click', function() {
-    
-  })
-
+  
+  // Submit poll to database
   $('#create-poll-form').on('submit', function(event) {
     const poll = {
       name: $('#decision').val(),
@@ -47,35 +45,43 @@ $(function() {
       desc: '',
       options: []
     };
-
+    
     $('.item-name').each((i, item) => {
       poll.options.push({ name: $(item).text() });
     });
     console.log(poll);
     $('<input>').attr('type', 'hidden')
-      .attr('name', 'poll')
-      .attr('value', JSON.stringify(poll))
-      .appendTo($('#create-poll-form'));
+    .attr('name', 'poll')
+    .attr('value', JSON.stringify(poll))
+    .appendTo($('#create-poll-form'));
     return true;
   });
+  
+  // Delete button for options on create page
+  $('ul').on('click', '.delete-option', function(event) {
+    $(event.currentTarget).closest('li').remove();
+   })
 
 
 
-
-  function pollVoteCreator() {
-    // creates the /:id table from database
-    // for each option, append below to <ul class"list-group id="sortable" on /:id
-    // <li>".list-group-item grouped-list ui-state-default"
-    // <div>".row line-div"
-    // <div>".col name-div"
-    //   <span>".item ui-icon""name from database"
-    // <div>".col desc-div"
-    //   <span>".ui-icon""desc from database"
-    // <div>".col current-rank"
-    //   <span>".ui-icon""display current rank"
-    //figure out way to track current position for this span
-    //$('.item') dom query?
-  }
+   // creates the /:id table from database
+  // function pollVoteCreator(database) {
+  //   const $list = $('<li>').addClass('list-group-item grouped-list ui-state-default');
+  //   const $divRow = $('<div>').addClass('row');
+  //   const $divColName = $('<div>').addClass('col');
+  //   const $spanName = $('<span>').addClass('item ui.icon item-name');
+    
+  //   $spanName.text(/*from database somehow*/);
+    
+  //   $divColName.append($spanName);
+  //   $divRow.append($divColName);
+  //   $list.add($divRow);
+  //   $list.apend('#sortable');
+    
+  // }
+  
+  // Votearranger/ranker on /:id
+  $('#sortable').sortable();
 
   // :id/results table creator. Identical to pollVoteCreator except for database source?
   function pollResultCreator(votes) {
@@ -94,7 +100,5 @@ $(function() {
     //$('.item') dom query?
   };
 
-  // Votearranger/ranker on /:id
-  $('#sortable').sortable();
 
 });
