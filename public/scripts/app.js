7e4d067682b5152ab642ff82d2d56b7f80b31e48
cjwsstrm <cjwesstrom@gmail.com>
2017-09-29 00:32:51 +0000

    Improved styling of ejs files and created base functions for voting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,7 +18,46 @@ $(function() {
    })
 
 
-//Votearranger on /:id
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
+   // Starter-function for displaying options made by user
+  function renderOptions (options) {
+    var optionsContainer = $(".???");
+    optionsContainer.empty();
+    options.forEach(function(tweet) {
+      optionsContainer.append(/*something*/);
+    });
+  }
+
+ //Starter-function for add-poll options button
+ $("btn btn-success").on("submit", function() {
+  event.preventDefault();
+
+  })
+
+
+  //Votearranger/ranker on /:id
+  $( "#sortable" ).sortable();
+  function voteRank() {
+    $(".submit btn").on("submit", function() {
+  //consider .class names for all elements
+      $('.item').map((i, item) => $(item).text())
+    //above method will query the dom and return an array of
+    //each item's current position. We send this information to
+    //the db.
+    }
+  }
+
+
+  // :id (vote) table creator
+  function pollOptionsCreator(options) {
+    //create table based on poll options set by creator
+  }
+
+
+  // :id/results table creator
+  function pollResultCreator(votes) {
+        //create table based on currently cast votes
+  }
+
+
+
 });
