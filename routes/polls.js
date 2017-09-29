e143a8292e612993a009666dda31b6b398d0d91b
vagrant <kiel.strang@gmail.com>
2017-09-29 22:34:02 +0000

    Connect route to get voting page

--- routes/polls.js
+++ routes/polls.js
@@ -7,7 +7,7 @@ function makePollURL(length) {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < length; i++)
+  for(var i = 0; i < length; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
@@ -23,20 +23,24 @@ module.exports = (db) => {
     const poll = JSON.parse(req.body.poll);
     poll.url = makePollURL(8);
     db.createPoll(poll);
-    res.redirect(`/${poll.url}/links`);
+    res.redirect(`/polls/${poll.url}/links`);
   });
 
   router.get("/:id", (req, res) => {
-    //add vote to database
-    //calculate ranks from votes
-    //load option ranks
-    res.render('vote');
+    db.getPoll(req.params.id)
+      .then(poll => {
+        res.locals.poll = poll;
+        res.render('vote');
+      });
   });
 
 
   router.post("/:id", (req, res) => {
+    const ballot = JSON.parse(req.body.poll);
+    console.log(ballot);
+    // db.saveBallot(req.params.id, ballot);
     //submit vote
-    res.redirect(`/${req.params.id}/results`);
+    res.redirect(`/polls/${req.params.id}/results`);
   });
 
   router.get("/:id/links", (req, res) => {
