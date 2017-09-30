f6e0d886cfac39982f5a0cf1f1e34bcc8b3d4108
vagrant <kiel.strang@gmail.com>
2017-09-30 18:37:42 +0000

    Add server-side poll input validation

--- routes/error-handler.js
+++ routes/error-handler.js
@@ -3,8 +3,11 @@ const errors = require('../lib/custom-errors.js');
 
 module.exports = (err, req, res, next) => {
   console.error('Error:', err.message);
-  if(err instanceof errors.PollNotFound || err instanceof errors.InvalidPollData) {
+  if(err instanceof errors.PollNotFound) {
     return res.status(err.status).send(err.message);
+  }
+  if(err instanceof errors.InvalidPollData) {
+    return res.status(err.status).send(JSON.stringify(err.pollErrors));
   }
   return res.status(500).send('An internal error occured');
 };
