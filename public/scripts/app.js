01bfbfa11bf49f6c433b5f4f7ee29d041a8ae2a0
cjwsstrm <cjwesstrom@gmail.com>
2017-10-01 02:28:42 +0000

    introduced basic error messages and added styling to error/alert messages

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,51 +6,108 @@ $(function() {
 
   $('#create-option-form').on('submit', function(event) {
     event.preventDefault();
-    const $list = $('<li>').addClass('list-group-item list-option');
-    const $divRowMain = $('<div>').addClass('row');
-    const $divColName = $('<div>').addClass('col');
-    const $spanName = $('<span>').addClass('item-name');
-    const $divColDelete = $('<div>').addClass('col');
-    const $buttonDelete = $('<i>').addClass('list-buttons fa fa-minus-square-o fa-2x delete-option');
-    const $divRowDesc = $('<div>').addClass('row');
-    const $smallDesc = $('<small>').addClass('item-desc');
+    var $optionNameLength = $('.add-option-name').val();
+    var $optionDescLength = $('.add-option-desc').val();
 
-    $spanName.text($('.add-option-name').val());
-    $smallDesc.text($('.add-option-desc').val());
+    if ($optionNameLength === "") {
+      swal({
+        title: 'Sorry!',
+        text:  'You need to add an Option Name',
+        icon:  'error'
+      });
+      return;
+    } if ($optionNameLength.length > 50) {
+      swal({
+        title: 'Sorry!',
+        text:  'Name too long, please make it shorter',
+        icon:  'error'
+      });
+      return;
+    } if ($optionDescLength.length > 150) {
+      swal ({
+        title: 'Sorry!',
+        text:  'Description too long, please make it shorter',
+        icon:  'error'
+      });
+      return;
+    } else {
+      var $list = $('<li>').addClass('list-group-item list-option');
+      var $divRowMain = $('<div>').addClass('row');
+      var $divColName = $('<div>').addClass('col');
+      var $spanName = $('<span>').addClass('item-name');
+      var $divColDelete = $('<div>').addClass('col');
+      var $buttonDelete = $('<i>').addClass('list-buttons fa fa-minus-square-o fa-2x delete-option');
+      var $divRowDesc = $('<div>').addClass('row');
+      var $smallDesc = $('<small>').addClass('item-desc');
 
-    $divColName.append($spanName);
-    $divColDelete.append($buttonDelete);
-    $divRowMain.append($divColName, $divColDelete);
-    $divRowDesc.append($smallDesc);
-    $list.append($divRowMain, $divRowDesc);
-    $list.insertBefore('.list-add-option');
+      $spanName.text($('.add-option-name').val());
+      $smallDesc.text($('.add-option-desc').val());
 
-    $('.add-option-name').val('');
-    $('.add-option-desc').val('');
+      $divColName.append($spanName);
+      $divColDelete.append($buttonDelete);
+      $divRowMain.append($divColName, $divColDelete);
+      $divRowDesc.append($smallDesc);
+      $list.append($divRowMain, $divRowDesc);
+      $list.insertBefore('.list-add-option');
+
+      $('.add-option-name').val('');
+      $('.add-option-desc').val('');
+    }
   })
 
   
   // Submit poll to database
   $('#create-poll-form').on('submit', function(event) {
-    const poll = {
-      name: $('#poll-name').val(),
-      email: $('#email').val(),
-      desc: $('#poll-desc').val(),
-      options: []
-    };
-    
+    var $namelength = $('#poll-name').val();
+    var $emailLength = $('#email').val();
+    var $descLength = $('#poll-desc').val();
+    if ($namelength === "") {
+      swal({
+        title: 'Sorry!',
+        text:  'You need to add a List Name',
+        icon:  'error'
+      });
+      event.preventDefault();
+      return;
+    } if ($emailLength === "") {
+      swal({
+        title: 'Sorry!',
+        text:  'Please add an email before submitting',
+        icon:  'error'
+      });
+      event.preventDefault();
+      return;
+    // } if ($namelength.length > 50) {
+      //     swal('Poll Name too long, please make it shorter');
+      //     return;
+    } if ($descLength.length > 150) {
+      swal({
+          title: 'Sorry!',
+          text:  'Poll Description too long, please make it shorter',
+          icon:  'error'
+        });
+        event.preventDefault();
+        return;
+    } else {
+        var poll = {
+          name: $('#poll-name').val(),
+          email: $('#email').val(),
+          desc: $('#poll-desc').val(),
+          options: []
+        };
+        
+        $('.list-option').each((i, item) => {
+          poll.options.push({ name: $(item).find('.item-name').text(),
+                              desc: $(item).find('.item-desc').text()
+                            });
+        });
 
-    $('.list-option').each((i, item) => {
-      poll.options.push({ name: $(item).find('.item-name').text(),
-                          desc: $(item).find('.item-desc').text()
-                         });
-    });
-
-    $('<input>').attr('type', 'hidden')
-    .attr('name', 'poll')
-    .attr('value', JSON.stringify(poll))
-    .appendTo($('#create-poll-form'));
-    return true;  
+        $('<input>').attr('type', 'hidden')
+        .attr('name', 'poll')
+        .attr('value', JSON.stringify(poll))
+        .appendTo($('#create-poll-form'));
+        return true;  
+    }
   });
   
   // Delete button for options on create page
