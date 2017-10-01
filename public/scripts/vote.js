3b5dc90ff7a87c9f6f20cbaaf51b8ec4381a191b
cjwsstrm <cjwesstrom@gmail.com>
2017-10-01 19:46:51 +0000

    added name requirement for voting

--- public/scripts/vote.js
+++ public/scripts/vote.js
@@ -1,14 +1,47 @@
 $(function() {
-  $('#vote-poll-form').on('submit', function(event) {
-    var ballot = [];
-    $('.item').each(function(i, item) {
-      ballot.push($(item).text());
-    });
-    $('<input>').attr('type', 'hidden')
-      .attr('name', 'ballot')
-      .attr('value', JSON.stringify(ballot))
-      .appendTo($('#vote-poll-form'));
-    return true;
-  });
   $('#sortable').sortable(); 
+  $('#vote-poll-form').on('submit', function(event) {
+    var $voterName = $('#voter-name').val();
+    if ($voterName === '') {
+      swal({
+        title: 'Chomp!',
+        text:  'You need to provide a name/alias/callsign',
+        icon:  'warning'
+      });
+      event.preventDefault();
+    } else {
+      var ballot = {
+       votername: $('#voter-name').val(),
+       options: []
+      };
+      $('.item').each(function(i, item) {
+        ballot.options.push($(item).text());
+      });
+      $('<input>').attr('type', 'hidden')
+        .attr('name', 'ballot')
+        .attr('value', JSON.stringify(ballot))
+        .appendTo($('#vote-poll-form'));
+      return true;
+    };
+  });
 });
+
+
+// var poll = {
+//   name: $('#poll-name').val(),
+//   email: $('#email').val(),
+//   desc: $('#poll-desc').val(),
+//   options: []
+// };
+
+// $('.list-option').each((i, item) => {
+//   poll.options.push({ name: $(item).find('.item-name').text(),
+//                       desc: $(item).find('.item-desc').text()
+//                     });
+// });
+
+// $('<input>').attr('type', 'hidden')
+// .attr('name', 'poll')
+// .attr('value', JSON.stringify(poll))
+// .appendTo($('#create-poll-form'));
+// return true;  
