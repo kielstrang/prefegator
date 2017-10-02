bbc606c14d57b3224c5705c89ac1e071c36c2448
Isaac Vallee <isaacvallee@gmail.com>
2017-10-02 00:38:15 +0000

    add list of voters

--- public/scripts/results.js
+++ public/scripts/results.js
@@ -1,3 +1,9 @@
 $(function() {
      $('.item-desc').first().show();
+
+     // Toggles voters list
+     $('.voters-list').hide();
+     $('#voters').on('mouseup', function(event) {
+        $('.voters-list').slideToggle();
+      });
 })
